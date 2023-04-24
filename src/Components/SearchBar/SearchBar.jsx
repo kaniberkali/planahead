e5b3b99a9539974324812f73cbd4e4eab35554bf
Muhammed Emin Ayar <eminayar142000@gmail.com>
2023-04-24 12:34:31 +0300

    Basic design has been prepared.

---
+++ src/Components/SearchBar/SearchBar.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import './searchbar.css'
+
+function searchBar() {
+  return (
+    <div id='searchBar'>
+        <input id='search' placeholder='search...'></input>
+    </div>
+  )
+}
+
+export default searchBar
