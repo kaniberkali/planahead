63922df1a5507fde842b3c4605291145cca5b802
Ali Kanıberk <kaniberkali>
2023-04-25 22:43:33 +0300

    mysql2 and sequalize module downloaded. Foundation laid for mysql connections

---
+++ server/db.js
@@ -0,0 +1,36 @@
+const express = require('express');
+const { Sequelize, Model, DataTypes } = require('sequelize');
+
+require('dotenv').config()
+
+const sequelize = new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PASSWORD, {
+    host: process.env.DB_HOST,
+    port: parseInt(process.env.DB_PORT),
+    dialect: 'mysql',
+    pool: {
+        max: parseInt(process.env.DB_MAX),
+        min: parseInt(process.env.DB_MIN),
+        acquire: parseInt(process.env.DB_ACQUIRE),
+        idle: parseInt(process.env.DB_IDLE)
+    }
+})
+
+const User = sequelize.define('user', {
+    name: DataTypes.STRING,
+    surname: DataTypes.STRING,
+    email: DataTypes.STRING,
+    password: DataTypes.STRING,
+    photo: DataTypes.STRING,
+    createdAt: {
+        type: DataTypes.DATE,
+        defaultValue: Sequelize.NOW
+    },
+    updatedAt: {
+        type: DataTypes.DATE,
+        defaultValue: Sequelize.NOW
+    }
+})
+
+sequelize.sync({ force: false })
+
+module.exports = { User }
--- server/index.js
+++ server/index.js
@@ -2,13 +2,17 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const app = express();
 const pretty = require('express-prettify');
+const { User} = require('./db.js');
 
-app.use(bodyParser.urlencoded({extended: true}));
+app.use(bodyParser.urlencoded({extended: true}))
 app.use(bodyParser.json());
-app.use(pretty({ always: true }));
+app.use(pretty({ always: true }))
 require('dotenv').config()
 app.use("/api", (req,res) => {
    res.send(process.env);
-});
-
-app.listen(process.env.PORT, () => console.log(`Server listening on port ${process.env.PORT}`));
+})
+(async () => {
+   const users = await User.findAll();
+   console.log(users)
+})()
+app.listen(process.env.PORT, () => console.log(`Server listening on port ${process.env.PORT}`))
