ec07584e05403572c4287f8e841ee216ecdd7e45
Ali Kanıberk <kaniberkali>
2023-04-25 18:07:30 +0300

    package.json edited

--- server/index.js
+++ server/index.js
@@ -2,7 +2,6 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const app = express();
 const pretty = require('express-prettify');
-const PORT = process.env.PORT || 3001;
 
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(bodyParser.json());
@@ -12,27 +11,4 @@ app.use("/api", (req,res) => {
    res.send(process.env);
 });
 
-app.get('/api/books', (req, res) => {
-    res.json(books);
-});
-
-app.post('/api/books', (req, res) => {
-    const book = req.body;
-    book.id = books.length + 1;
-    books.push(book);
-    res.json(book);
-});
-
-app.put('/api/books/:id', (req, res) => {
-    const id = parseInt(req.params.id);
-    const book = req.body;
-    const index = books.findIndex(b => b.id === id);
-    if (index !== -1) {
-        books[index] = book;
-        res.json(book);
-    } else {
-        res.status(404).send('Book not found');
-    }
-});
-
-app.listen(PORT, () => console.log(`Server listening on port ${PORT}`));
+app.listen(process.env.PORT, () => console.log(`Server listening on port ${process.env.PORT}`));
