81406e6b4095fd488755a0f6743a85af965c5f92
Ali Kanıberk <kaniberkali>
2023-05-06 22:18:22 +0300

    register, login, session, logout request points was created

--- server/index.js
+++ server/index.js
@@ -5,6 +5,7 @@ const pretty = require('express-prettify')
 const { User} = require('./db.js')
 const {register, login} = require("./func.js")
 const router = express.Router()
+const session = require('express-session');
 
 app.use(bodyParser.urlencoded({extended: true}))
 app.use(bodyParser.json());
@@ -12,11 +13,37 @@ app.use(pretty({ always: true }))
 require('dotenv').config()
 app.use('/api', router);
 
-router.post("/register", (req,res) => {
-   res.send(register(req.body));
+app.use(session({
+   secret: process.env.SESSION_SECRET,
+   resave: false,
+   saveUninitialized: true
+}))
+
+router.post("/register", async (req,res) => {
+   const isRegister = await register(req.body)
+   if (isRegister)
+      req.session.user = { username: req.body.username, password: req.body.password };
+   res.send(isRegister);
 })
 
-router.post("/login", (req, res) => {
-   res.send(login(req.body))
+router.post("/login", async (req, res) => {
+   const isLogin = await login(req.body)
+   if (isLogin)
+      req.session.user = { username: req.body.username, password: req.body.password }
+   res.send(isLogin)
 })
+
+router.get("/session",  (req, res) => {
+   res.send(req.session.user)
+})
+
+router.get("/logout", (req, res) => {
+   if (req.session.user) {
+      req.session.destroy()
+      res.send(true)
+   }
+   else
+      res.send(false)
+})
+
 app.listen(process.env.PORT, () => console.log(`Server listening on port ${process.env.PORT}`))
