3a4261a7bfe8b01f600cfc2103cb76e61da7b324
Ali Kanıberk <kaniberkali>
2023-05-07 01:51:05 +0300

    cors error fixed

--- server/index.js
+++ server/index.js
@@ -8,16 +8,16 @@ const { userRegisterSchema } = require("./validation.js")
 const router = express.Router()
 const session = require('express-session');
 require('dotenv').config()
+const cors = require('cors');
+
+
+app.use(cors({
+   origin: process.env.FRONTEND_URL
+}))
 app.use(bodyParser.urlencoded({extended: true}))
 app.use(bodyParser.json());
 app.use(pretty({ always: true }))
 app.use('/api', router);
-const cors = require('cors');
-
-app.use(cors({
-   origin: process.env.FRONTEND_URL,
-   optionsSuccessStatus: 200
-}))
 
 app.use(session({
    secret: process.env.SESSION_SECRET,
@@ -25,17 +25,17 @@ app.use(session({
    saveUninitialized: true
 }))
 
-router.post("/register", validateRequestBody(userRegisterSchema),async (req,res) => {
+router.post("/register",async (req,res) => {
    const isRegister = await register(req.body)
-   if (isRegister)
-      req.session.user = { username: req.body.username, password: req.body.password };
+   if (isRegister && req.session)
+      req.session.user = { email: req.body.email, password: req.body.password };
    res.send(isRegister);
 })
 
 router.post("/login", async (req, res) => {
    const isLogin = await login(req.body)
    if (isLogin)
-      req.session.user = { username: req.body.username, password: req.body.password }
+      req.session.user = { email: req.body.email, password: req.body.password }
    res.send(isLogin)
 })
 
--- server/validation.js
+++ server/validation.js
@@ -1,10 +1,11 @@
 const Yup = require('yup');
 
 const userRegisterSchema = Yup.object().shape({
-    username:Yup.string().required('Bu alan zorunludur').matches(/^[aA-zZ\s]+$/, "Yanlızca alfabetik karakter kullanılabilir."),
+    name:Yup.string().required('Bu alan zorunludur').matches(/^[aA-zZ\s]+$/, "Yanlızca alfabetik karakter kullanılabilir."),
+    surname:Yup.string().required('Bu alan zorunludur').matches(/^[aA-zZ\s]+$/, "Yanlızca alfabetik karakter kullanılabilir."),
     password:Yup.string().required('Bu alan zorunludur').min(8, 'Şifre en az 8 karakterli olabilir.')
         .matches(/^[aA-zZ\s]+$/, "Şifrede yanlızca alfabetik karakterler olmalıdır..."),
     email:Yup.string().email('Lütfen bir email adresi girin...').required('Bu alan zorunludur'),
 });
 
-module.export = {userRegisterSchema}
+module.export = { userRegisterSchema }
