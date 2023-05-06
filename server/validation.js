748c7f4e55e03fd5491b65e55ebe6f7c862e7252
Ali Kanıberk <kaniberkali>
2023-05-06 22:46:27 +0300

    validation folder edited

--- server/validation.js
+++ server/validation.js
@@ -1,6 +1,11 @@
 const express = require('express');
-import * as yup from 'yup';
+import * as Yup from 'yup';
 
-const userLoginScheme = yup.object().shape({
-    name: yup.string().min(1).required()
+const userLoginScheme = Yup.object().shape({
+    username:Yup.string().required('Bu alan zorunludur').matches(/^[aA-zZ\s]+$/, "Yanlızca alfabetik karakter kullanılabilir."),
+    password:Yup.string().required('Bu alan zorunludur').min(8, 'Şifre en az 8 karakterli olabilir.')
+        .matches(/[a-zA-Z]/, 'Password can only contain Latin letters.'),
+    email:Yup.string().email().required('Bu alan zorunludur'),
 })
+
+module.exports = { userLoginScheme }
